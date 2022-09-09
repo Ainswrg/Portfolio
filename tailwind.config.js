b2fe982b300b0f743a39f53f443fae6a98057628
Jaslan <Jaslan.dlnv@gmail.com>
2022-09-09 15:39:32 +0500

    refactor: remove scss add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    fontSize: {
+      '4.5xl': '2.5rem',
+    },
+    extend: {
+      colors: {
+        light: 'rgba(255, 255, 255, 0.6)',
+        bg: '#1f1f38',
+        bgVariant: '#2c2c6c',
+        primary: '#4db5ff',
+        primaryVariant: 'rgba(77, 181, 255, 0.4)',
+        white: '#fff',
+      },
+      transitionDuration: {
+        0: '0ms',
+        400: '400ms',
+      },
+      backgroundImage: {
+        'bg-texture': "url('./assets/bg-texture.png')",
+      },
+    },
+  },
+  plugins: [],
+};
