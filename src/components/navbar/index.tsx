fad5b28e1f6fe0aceddc1aa6ac6147775e165116
Jaslan <Jaslan.dlnv@gmail.com>
2022-09-10 17:09:24 +0500

    feat: add navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,62 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import styles from './Navbar.module.scss';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { GiBookshelf } from 'react-icons/gi';
+import { RiServiceLine } from 'react-icons/ri';
+import { TiMessages } from 'react-icons/ti';
+
+import { navbar } from '../../style';
 
 export const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = React.useState('#');
+
+  const onClickHandler: (e: any) => void = (e) => {
+    setActive(e);
+  };
+
+  return (
+    <nav className={`${navbar.root}`}>
+      <a
+        className={`${navbar.link} ${active === '#' ? navbar.active : ''}`}
+        href="#"
+        onClick={() => onClickHandler('#')}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        className={`${navbar.link} ${active === '#about' ? navbar.active : ''}`}
+        href="#about"
+        onClick={() => onClickHandler('#about')}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        className={`${navbar.link} ${
+          active === '#experience' ? navbar.active : ''
+        }`}
+        href="#experience"
+        onClick={() => onClickHandler('#experience')}
+      >
+        <GiBookshelf />
+      </a>
+      <a
+        className={`${navbar.link} ${
+          active === '#services' ? navbar.active : ''
+        }`}
+        href="#services"
+        onClick={() => onClickHandler('#services')}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        className={`${navbar.link} ${
+          active === '#contacts' ? navbar.active : ''
+        }`}
+        href="#contacts"
+        onClick={() => onClickHandler('#contacts')}
+      >
+        <TiMessages />
+      </a>
+    </nav>
+  );
 };
--- src/style.ts
+++ src/style.ts
@@ -11,4 +11,9 @@ export const header = {
     'items-center mt-8 gap-3 flex-col absolute left-0 bottom-12 after:w-px after:h-8 after:bg-primary hidden sm:flex',
 };
 
+export const navbar = {
+  root: 'bg-[rgba(0,0,0,0.3)] flex w-max px-7 py-3 fixed z-10 left-1/2 -translate-x-1/2 bottom-8 gap-3 rounded-[3rem] backdrop-blur-lg',
+  link: 'bg-transparent p-[0.9rem] rounded-full flex text-light text-lg hover:bg-[rgba(0,0,0,0.3)]',
+  active: 'bg-bg text-white',
+};
 export default styles;
